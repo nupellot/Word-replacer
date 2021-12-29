4f80c5fb1a23273af67cca2031f58eadb8650464
Dmitriy Korotkov <54524404+nupellot@users.noreply.github.com>
2021-12-29 15:51:24 +0000

    Реализован главный функционал

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 #include <time.h>
 #include <string.h>
 
+#define MIN_WORDS 10
+
 
 int main(void) {
   FILE* base = fopen("base", "r+");
-	FILE* source = fopen("source", "w");
-	if (base == NULL || source == NULL) {
+	if (base == NULL) {
 		printf("Ошибка открытия файла\n");
 		exit(1);
 	}
@@ -16,7 +17,7 @@ int main(void) {
 	srand(time(NULL));
 	// Объявляем используемые функции.
 	char getRandomPunctuationMark();
-
+	void replace(int amountOfSourceWords, char** sourceWords);
 	
 	char c;
 	// Подсчитываем количество слов в базе данных.
@@ -24,7 +25,6 @@ int main(void) {
 	while ((c = fgetc(base)) != EOF && c != '#') {
 		if (c == '\n') amountOfBaseWords++;
 	}
-	printf("%d\n", amountOfBaseWords);
 
 	// Выделение памяти под массив слов из базы данных.
 	char** baseWords = (char**)malloc(sizeof(char) * 50 * amountOfBaseWords);
@@ -38,31 +38,46 @@ int main(void) {
 	for (int i = 0; i < amountOfBaseWords; i++) {
 		fscanf(base, "%s\n", baseWords[i]);
 	}
+	fclose(base);
 
-	// Вывод считанных слов из базы данных.
-	for (int i = 0; i < amountOfBaseWords; i++) {
-		printf("%s\n", baseWords[i]);
-	}	
-
+	// Получаем от пользователя требуемое количество слов в исходнике.
 	printf("Введите требуемое количество слов в файле:\n");
 	int amountOfSourceWords;
 	scanf("%d", &amountOfSourceWords);
-	for (int i = 0; i < amountOfSourceWords; i++) {
-		fprintf(source, "%s", baseWords[rand() % amountOfBaseWords]);
-		if (rand() % 5 == 0) fprintf(source, "%c", getRandomPunctuationMark());
-		fprintf(source, " ");
+	if (amountOfSourceWords < MIN_WORDS) {
+		printf("Слишком мало слов, введите больше\n");
+		exit(2);
 	}
 
-	FILE* replacer = fopen("replacer", "r+");
-	if (replacer == NULL) {
+	// Выделение памяти под массив слов в исходнике.
+	char** sourceWords = (char**)malloc(sizeof(char) * 50 * amountOfSourceWords);
+	for (int i = 0; i < amountOfSourceWords; i++) {
+		// Под каждое конкретное слово в массиве.
+		sourceWords[i] = (char*)malloc(sizeof(char) * 50);
+	}
+	// Создание массива слов в исходнике.
+	for (int i = 0; i < amountOfSourceWords; i++) {
+		sourceWords[i] = baseWords[rand() % amountOfBaseWords];
+	}
+
+  replace(amountOfSourceWords, sourceWords);
+
+	// Открываем исходник для записи в него данных.
+	FILE* source = fopen("source", "w");
+	if (source == NULL) {
 		printf("Ошибка открытия файла\n");
 		exit(1);
 	}
-	char replaced[50];
-	char replacing[50];
-	fscanf(replacer, "%s", replaced);
-	fscanf(replacer, "%s", replacing);
-	printf("%s\n%s\n", replaced, replacing);
+	// Записывае в файл измененный массив слов.
+	for (int i = 0; i < amountOfSourceWords; i++) {
+		fprintf(source, "%s", sourceWords[i]);
+		if (rand() % 5 == 0) fprintf(source, "%c", getRandomPunctuationMark());
+		fprintf(source, " ");
+	}
+	fclose(source);
+
+	free(baseWords);
+	free(sourceWords);
 }
 
 // Генератор случайного знака препинания.
@@ -74,4 +89,25 @@ char getRandomPunctuationMark() {
 	if (n == 3) return '!';
 	if (n == 4) return '?';
 	return -1;
+}
+
+void replace(int amountOfSourceWords, char** sourceWords) {
+	// Забираем из файла пару слов (заменяемое - заменитель).
+	FILE* replacer = fopen("replacer", "r+");
+	if (replacer == NULL) {
+		printf("Ошибка открытия файла\n");
+		exit(1);
+	}
+	char replaced[50];
+	char replacing[50];
+	fscanf(replacer, "%s", replaced);
+	fscanf(replacer, "%s", replacing);
+	fclose(replacer);
+
+	// Производим замену слов.
+	for (int i = 0; i < amountOfSourceWords; i++) {
+		if (strcmp(sourceWords[i], replaced) == 0) {
+			strcpy(sourceWords[i], replacing);
+		}
+	}
 }
